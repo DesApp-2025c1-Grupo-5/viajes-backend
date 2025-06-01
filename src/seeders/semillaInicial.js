127bfc91d51a8e9be727f93e370ce545e0800607
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2025-06-01 02:51:22 -0300

    Se cambiaron nombres de variables para normalizarlo con la bd y el front. Se agregó un vaciado de la tabla de empresa transportista en la semilla al llenarla nuevamente para eliminar registros previos

--- src/seeders/semillaInicial.js
+++ src/seeders/semillaInicial.js
@@ -2,34 +2,37 @@ const {EmpresaTransportista} = require('../models/')
 
 const crearValoresInicialesDB = async ()=>{
     try{
+
+        await EmpresaTransportista.destroy({ truncate: true })
+
         const empresa1 = await EmpresaTransportista.create({
-            razonSocial: "Transportes Rápidos S.A",
+            razon_social: "Transportes Rápidos S.A",
             cuit_rut: "20123456789",
             email:"correoprueba@gmail.com",
             telefono:"+51 987654321",
             pais:"Argentina",
             provincia_estado:"Buenos Aires",
-            domicilioFiscal:"No especificado",
+            domicilio_fiscal:"No especificado",
             observaciones:"Hola! soy una observacion"
         });
         const empresa2 = await EmpresaTransportista.create({
-            razonSocial: "LogiExpress",
+            razon_social: "LogiExpress",
             cuit_rut: "20-98765432-1",
             email:"info@logiexpress.com",
             telefono:"+51 912345678",
             pais:"Argentina",
             provincia_estado:"Buenos Aires",
-            domicilioFiscal:"Av. Emilio castro 5409",
+            domicilio_fiscal:"Av. Emilio castro 5409",
             observaciones:"Aguante solo leveling"
         });
         const empresa3 = await EmpresaTransportista.create({
-            razonSocial: "CargoMax",
+            razon_social: "CargoMax",
             cuit_rut: "20-45678912-3",
             email:"contacto@cargomax.com",
             telefono:"+51 945678123",
             pais:"Argentina",
             provincia_estado:"CABA",
-            domicilioFiscal:"CABA",
+            domicilio_fiscal:"CABA",
             observaciones:"Muerte hunter x hunter"
         });
     }catch(error){
